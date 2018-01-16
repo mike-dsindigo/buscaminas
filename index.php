fee0019d428c173e4fb6f3bae58c5b12bf96db54
Miguel Martínez Varona <miguel.martinez.va@gmail.com>
2018-01-16 11:56:58 -0600

    Modificación de estructura e integración de bibliotecas externas e internas :construction:
    jQuery + Popper + Bootstrap4 además de hoja de estilos y script js personalizados

--- index.php
+++ index.php
@@ -3,7 +3,26 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Buscaminas</title>
+
+    <!-- JQuery -->
+    <script src="./js/jquery-3.2.1.slim.min.js"></script>
+    <!-- Popper -->
+    <script src="./js/popper.min.js"></script>
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <script src="./js/bootstrap.js"></script>
+
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/buscaminas.js"></script>
 </head>
 <body>
+    <header>
+        <div class="row justify-content-md-center">
+            <h1>Buscaminas</h1>
+        </div>
+    </header>
+    <footer>
+        By Mike Mtz
+    </footer>
 </body>
 </html>
