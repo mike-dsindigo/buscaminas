14536fa4c8a14a51abee0403448af057f875d1d8
Miguel Martínez Varona <miguel.martinez.va@gmail.com>
2018-01-16 16:24:40 -0600

    Personalización y optimización v1 :rocket:
    El código se ha optimizado y se ha definido un código de colores para cada valor en el tablero de resultados

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -1,18 +1,30 @@
 'use strict';
 $(function() {
+    //Inicializar variables
     let columnas = $("#columnas").val();
     let filas = $("#filas").val();
+    /**
+     * Acción detonada al cambiar el número de columnas
+     */
     $("#columnas").change(function () {
         columnas = $(this).val();
         drawMinas(filas, columnas);
     });
+    /**
+     * Acción detonada al cambiar el número de filas
+     */
     $("#filas").change(function () {
         filas = $(this).val();
         drawMinas(filas, columnas);
     });
 
+    /**
+     * Función que traza los tableros dependiendo del número de filas y columnas que se envíen
+     * @param filas
+     * @param columnas
+     */
     let drawMinas = function(filas, columnas) {
-        if(filas < 1 || columnas < 1){
+        if(filas < 1 || columnas < 1){ //Matriz no válida
             $("#table_minas").hide();
             $("#table_result").hide('fast');
             return
@@ -31,7 +43,7 @@ $(function() {
                                     "<i class='far fa-circle'></i>" +
                                 "</button>"+
                               "</td>";
-                html_result += "<td><span id='res_" + i + "_" + j + "'>0</span></td>";
+                html_result += "<td id='td_" + i + "_" + j + "'><span id='res_" + i + "_" + j + "'>0</span></td>";
             }
             html_minas += "</tr>";
             html_result += "</tr>";
@@ -42,75 +54,72 @@ $(function() {
     }
 });
 
+/**
+ * Función que cambia el status del botón
+ * @param btn_id
+ */
 let changeMina = function(btn_id) {
     let element = $("#" + btn_id);
     let result = $("#res_" + btn_id);
+    let td = $("#td_" + btn_id);
+    td.attr('class', '');
 
     let x = element.attr('x');
     let y = element.attr('y');
     let status = element.attr('status');
 
     if(status == 'off') {
-        element.addClass('btn-danger').html("<i class='fas fa-flag'></i>").attr('status', 'on');
-        result.html("<i class='fas fa-flag'></i>");
-        addMina(x, y);
+        element.addClass('btn-danger').html("<i class='fas fa-bomb'></i>").attr('status', 'on');
+        result.html("<i class='fas fa-bomb'></i>").attr('class','').addClass('text_0');
+        td.addClass('bg-danger');
+        setMina(x, y, 'add');
     }else {
         element.removeClass('btn-danger').html("<i class='far fa-circle'></i>").attr('status', 'off');
-        removeMina(x, y);
+        setMina(x, y, 'remove');
     }
 };
 
-let addMina = function(x, y) {
-    x = parseInt(x);
-    y = parseInt(y);
-    for(let i = (x - 1); i <= (x + 1); i++){
-        for(let j = (y - 1); j <= (y + 1); j++){
-            if((i + "_" + j) != (x + "_" + y)) {
-                let item = $("#res_" + i + "_" + j);
-                if(parseInt(item.text())>=0){
-                    let value = parseInt(item.text()) + 1;
-                    item.html(value);
-                }
-            }
-        }
-    }
-};
-
-let removeMina = function(x, y) {
-    x = parseInt(x);
-    y = parseInt(y);
-    for(let i = (x - 1); i <= (x + 1); i++){
-        for(let j = (y - 1); j <= (y + 1); j++){
-            let item = $("#res_" + i + "_" + j);
-            if(parseInt(item.text())>=0){
-                let value = parseInt(item.text()) - 1;
-                item.html(value);
-            }
-        }
-    }
-    checkItem(x, y);
-};
-
-let checkItem = function(x, y) {
-    let item = $("#res_" + x + "_" + y);
+/**
+ *
+ * @param x
+ * @param y
+ * @param action
+ */
+let setMina = function(x, y, action) {
     x = parseInt(x);
     y = parseInt(y);
     let count = 0;
     for(let i = (x - 1); i <= (x + 1); i++){
-        if( i>=0) {
-            for (let j = (y - 1); j <= (y + 1); j++) {
-                if( j>=0 ) {
+        for(let j = (y - 1); j <= (y + 1); j++){
+            let item = $("#res_" + i + "_" + j);
+            switch (action){
+                case 'add':
+                    if((i + "_" + j) != (x + "_" + y)) {
+                        if(parseInt(item.text())>=0){
+                            let value = parseInt(item.text()) + 1;
+                            item.html(value).attr('class','').addClass('text_' + value);
+                        }
+                    }
+                break;
+                case 'remove':
+                    if(parseInt(item.text())>=0){
+                        let value = parseInt(item.text()) - 1;
+                        item.html(value).attr('class','').addClass('text_' + value);
+                    }
+                break;
+                case 'check':
                     if ((i + "_" + j) != (x + "_" + y)) {
-                        let item_check = $("#res_" + i + "_" + j);console.log();
-                        if (!(parseInt(item_check.text()) >= 0) && item_check[0])
+                        if (!(parseInt(item.text()) >= 0) && item[0])
                             count++;
                     }
-                }
+                break;
             }
         }
     }
-    item.html(count);
+    if(action=='remove')
+        setMina(x, y, 'check');
+    if(action=='check')
+        $("#res_" + x + "_" + y).html(count).attr('class','').addClass('text_' + count);
 };
-
 
 
